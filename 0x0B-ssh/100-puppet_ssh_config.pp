3d337d941b45e7311ef7ffe0361439733b0fe9ba
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-12-12 17:43:00 +0100

    restoring

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# setup client SSH configuration
+# Bash script to setup client SSH configuration
 
 exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
