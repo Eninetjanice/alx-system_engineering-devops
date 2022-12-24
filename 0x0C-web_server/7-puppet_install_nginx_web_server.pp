a2e3835306eb06090292b7d67f72c3794393b88d
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-12-24 07:23:50 +0100

    Made changes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,21 @@
 # Install Nginx web server (w/ Puppet)
-exec { 'server configuration':
-  provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+
+exec { 'nginx install':
+path    => '/usr/bin',
+command => 'sudo apt update && sudo apt-get -y install nginx',
+}
+
+file { '/var/www/html/index.html':
+mode    => '0755',
+content => 'Hello World!',
+}
+
+exec { 'block server':
+path    => '/usr/bin',
+command => 'sudo sed -i "/server_name _;/a location /redirect_me {\\n\\trewrite ^\/redirect_me 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; listen 80; \\n\\t}\\n" /etc/nginx/sites-enabled/default; sudo service nginx restart',
+}
+
+exec { 'Restart nginx':
+path    => '/usr/bin',
+command => 'sudo service nginx restart',
 }
